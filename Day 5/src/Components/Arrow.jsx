b03307b3a88dd1eabe1f57ff2f05b696abe0d201
NitinRDesai20 <148466355+NitinRDesai20@users.noreply.github.com>
2023-10-28 23:35:30 +0530

    Add files via upload

---
+++ Day 5/src/Components/Arrow.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Arrow.css";
+
+const Arrow = () => {
+  return (
+    <>
+      <h1 id="header" className="text-center">This is Arrow Functional component</h1>
+      <p>Lorem ipsum dolor sit amet?</p>
+      <input type="text" />
+    </>
+  );
+};
+
+export default Arrow;
---
+++ Day 5/src/Components/Cla.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Cla extends Component {
+  render() {
+    return (
+      <>
+        <h1>Class Components</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iste voluptas eligendi reiciendis explicabo, soluta facilis tempore voluptatem quia a libero corrupti sunt architecto consequuntur quis similique modi quo esse!
+        </p>
+      </>
+    )
+  }
+}
+
+export default Cla
